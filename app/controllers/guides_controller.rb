4cae999a57ce5f7d18d66d2faec579b0a761b246
anouar elkhalfi <anouarelkhalfi@gmail.com>
2024-06-17 17:37:35 +0200

    migration

---
+++ app/controllers/guides_controller.rb
@@ -0,0 +1,29 @@
+class GuidesController < ApplicationController
+  before_action :authenticate_user!
+  # before_action :ensure_user_is_guide, only: [:new, :create]
+
+  def new
+    @guide = current_user
+  end
+
+  def create
+    @guide = current_user
+    if @guide.update(guide_params)
+      redirect_to @guide, notice: 'Guide profile was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def guide_params
+    params.require(:user).permit(:title, :description, :location, :language_spoken, :profile_picture, :bio)
+  end
+
+  def ensure_user_is_guide
+    unless current_user.guide?
+      redirect_to root_path, alert: 'You must be a guide to access this section.'
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'guides/new'
+  get 'guides/create'
+  get 'guides/show'
+  get 'guides/index'
   devise_for :users
   # Other routes
   root to: 'pages#home'
+
+  resources :guides, only: [:new, :create, :show, :index]
 end
